84f07d61b9b618c4df681d0f930b245cf537e4eb
Jaani Singh <i.jatin@aol.com>
2021-02-15 23:07:10 -0500

    Final Submission modified Printing even numbers 0 to 100

--- Assignment01/src/assignment01/Assignment01.java
+++ Assignment01/src/assignment01/Assignment01.java
@@ -11,7 +11,7 @@ public class Assignment01 {
     
     public static void main(String[] args) {
         // Running loop to print all the even numbers from 0 to 100
-        for (int i=0; i< 100; i++){
+        for (int i=0; i<= 100; i++){
             
             if(i%2 ==0){
                 System.out.println(i);
